57fb4659bc3eb6ecbe9dbcf7389eb40d1ad598e1
Siddharth <siddharthisrani123@gmail.com>
2024-07-18 23:59:38 +0530

    Added connection of mongodb

--- server/server.js
+++ server/server.js
@@ -2,12 +2,16 @@ const express = require("express")
 const dotenv = require('dotenv');
 const cors = require("cors")
 const app = express()
+const connectDB = require('./config/db');
 
 // Middleware to parse JSON bodies
 app.use(express.json());
  
 // Load environment variables from .env file
 dotenv.config();
+
+// Connect to the database
+connectDB();
 
 app.use(cors());
 
